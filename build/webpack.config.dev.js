a86167205bc1fbc00fecec20b98612365fb604f8
Jan Max Hormiguera <Janh@MTB-099.Meditab.Local>
2020-02-03 18:52:38 +0800

    Initial commit

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true,
+    historyApiFallback: true
+    // watchOptions: {
+    //   poll: true
+    // }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: ['vue-style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: resolve('static'),
+        to: resolve('dist'),
+        toType: 'dir'
+      }
+    ]),
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        stylus: {
+          import: [path.resolve(__dirname, '../assets/helpers.styl')]
+        }
+      }
+    })
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '../assets/app.styl'
+
+/* eslint-disable-next-line no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
